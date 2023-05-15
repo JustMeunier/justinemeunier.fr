f6c91df36ac682fa6e85a3f9dfe5355025749f14
Justine Meunier <justine.meunier.design@gmail.com>
2023-05-16 00:03:16 +0200

    Btn Copier mail fixed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const btnEmail = document.getElementById('btnEmail');
 
 function updateClipboard(newClip) {
-    navigator.clipboard.write(newClip).then(() => {
+    navigator.clipboard.writeText(newClip).then(() => {
       /* clipboard successfully set */
     }, () => {
       /* clipboard write failed */
     });
   }
 
-  btnEmail.addEventListener('click', updateClipboard('justine.meunier.design@gmail.com'));
+  btnEmail.addEventListener('click', () => updateClipboard('justine.meunier.design@gmail.com'));
